592e65f1273e5a0af9c494d4db2b97c491e1070a
Travis Levi B <tbateswsu@gmail.com>
2022-06-15 16:15:29 -0700

    feat: added buttons to card for linking to repo and live page, class for hidden, and link around card for triggering event handler.

--- public/js/script.js
+++ public/js/script.js
@@ -51,11 +51,11 @@ const getReposTeamMembersGitHub = () => {
 
 getReposTeamMembersGitHub();
 
-function arrayRemove(arr, value) {
-  return arr.filter(function (ele) {
-    return ele != value;
-  });
-}
+// function arrayRemove(arr, value) {
+//   return arr.filter(function (ele) {
+//     return ele != value;
+//   });
+// }
 
-var result = arrayRemove(array, 6);
+// var result = arrayRemove(array, 6);
 // result = [1, 2, 3, 4, 5, 7, 8, 9, 0]
