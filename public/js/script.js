fb54f1d4cec4aff501ebfa6d4585f9433246c4e6
Travis Bates <tbateswsu@gmail.com>
2022-06-15 18:34:03 -0700

    feat: choosing nav to put on individual cards

--- public/js/script.js
+++ public/js/script.js
@@ -50,18 +50,3 @@ const getReposTeamMembersGitHub = () => {
 };
 
 getReposTeamMembersGitHub();
-
-// function arrayRemove(arr, value) {
-//   return arr.filter(function (ele) {
-//     return ele != value;
-//   });
-// }
-
-// var result = arrayRemove(array, 6);
-// result = [1, 2, 3, 4, 5, 7, 8, 9, 0]
-
-show_card_links_handler = (event) => {
-  event.preventDefault();
-  if (event.target.id === "card-link-btn") {
-  }
-};
